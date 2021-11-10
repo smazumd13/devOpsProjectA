567ebfaaad9557b445bc0458f177f88eb8b065d5
sourav mazumdar <sourav.mazumdar@bell.ca>
2021-11-10 14:05:21 +0530

    modified node config for ansible

--- jenkinsnode.sh
+++ jenkinsnode.sh
@@ -8,5 +8,6 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 sudo apt-get install -y python-properties
+sudo apt install python-pip
 sudo pip install ansible[azure]
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
